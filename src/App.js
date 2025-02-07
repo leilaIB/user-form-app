0203e2aee7f63767b1f521432ca46577889d16d7
leila <hafssabeen845@gmail.com>
2025-02-07 15:09:11 +0100

    Ajout des fichiers React et préparation du dossier backend

--- src/App.js
+++ src/App.js
@@ -1,23 +1,83 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+  const [user, setUser] = useState({
+    firstName: '',
+    lastName: '',
+    gender: '',
+    cin: '',
+    address: '',
+    dateOfBirth: ''
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setUser({ ...user, [name]: value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    alert('Formulaire soumis !');
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Formulaire Utilisateur</h1>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>Nom :</label>
+          <input type="text" name="firstName" value={user.firstName} onChange={handleChange}/>
+        </div>
+        <div>
+          <label>Prénom :</label>
+          <input
+            type="text"
+            name="lastName"
+            value={user.lastName}
+            onChange={handleChange}
+          />
+        </div>
+        <div>
+          <label>Sexe :</label>
+          <select
+            name="gender"
+            value={user.gender}
+            onChange={handleChange}
+          >
+            <option value="">Sélectionner...</option>
+            <option value="Homme">Homme</option>
+            <option value="Femme">Femme</option>
+          </select>
+        </div>
+        <div>
+          <label>CIN :</label>
+          <input
+            type="text"
+            name="cin"
+            value={user.cin}
+            onChange={handleChange}
+          />
+        </div>
+        <div>
+          <label>Adresse :</label>
+          <input
+            type="text"
+            name="address"
+            value={user.address}
+            onChange={handleChange}
+          />
+        </div>
+        <div>
+          <label>Date de naissance :</label>
+          <input
+            type="date"
+            name="dateOfBirth"
+            value={user.dateOfBirth}
+            onChange={handleChange}
+          />
+        </div>
+        <button type="submit">Ajouter Utilisateur</button>
+      </form>
     </div>
   );
 }
